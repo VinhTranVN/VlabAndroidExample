b745adb08a40455d5cd799a7bc31eb004e71e948
Vinh Tran <vit@tapptic.com>
2016-04-13 15:06:17 +0700

    init

---
+++ app/src/main/java/com/vlab/androidexample/MyApplication.java
@@ -0,0 +1,9 @@
+package com.vlab.androidexample;
+
+import android.app.Application;
+
+/**
+ * Created by vlab.smartapps@gmail.com on 4/13/16.
+ */
+public class MyApplication extends Application {
+}
---
+++ app/src/main/java/com/vlab/androidexample/activities/MainActivity.java
@@ -0,0 +1,18 @@
+package com.vlab.androidexample.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.vlab.androidexample.R;
+
+/**
+ * Created by vlab.smartapps@gmail.com on 4/13/16.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
